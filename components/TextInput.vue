4785d2e09033387af859b4a81c61b22b0730d0a2
pixsel1401 <pixsel1401@gmail.com>
2023-06-27 18:05:55 +0500

    add login register

--- components/TextInput.vue
+++ components/TextInput.vue
@@ -1,9 +1,6 @@
 <template>
     <div>
-        <input 
-            :id="`input-${placeholder}`"
-            :placeholder="placeholder"
-            class="
+        <input :id="`input-${placeholder}`" :placeholder="placeholder" class="
                 block
                 w-full
                 bg-[#F1F1F2]
@@ -14,30 +11,26 @@
                 py-2.5
                 px-3
                 focus:outline-none
-            " 
-            :type="inputType"
-            v-model="inputComputed"
-            autocomplete="off"
-            :maxlength="max"
-        >
+            " :type="inputType" v-model="inputComputed" autocomplete="off" :maxlength="max">
         <span v-if="error" class="text-red-500 text-[14px] font-semibold">
             {{ error }}
         </span>
     </div>
-                    
 </template>
 
-<script setup>
-const emit = defineEmits(['update:input'])
-    const props = defineProps(['input', 'placeholder', 'inputType', 'max', 'autoFocus', 'error'])
-    const { input, placeholder, inputType, max, autoFocus, error } = toRefs(props)
-    onMounted(() => {
-        if (autoFocus.value) {
-            document.getElementById(`input-${placeholder.value}`).focus()
-        }
-    })
-    const inputComputed = computed({
-        get: () => input.value,
-        set: (val) => emit('update:input', val)
-    })
+<script setup lang="ts">
+const emit = defineEmits<{
+    (event: 'update:input' , val : String) : String
+}>()
+const props = defineProps(['input', 'placeholder', 'inputType', 'max', 'autoFocus', 'error'])
+const { input, placeholder, inputType, max, autoFocus, error } = toRefs(props)
+onMounted(() => {
+    if (autoFocus?.value) {
+        document.getElementById(`input-${placeholder?.value}`)?.focus()
+    }
+})
+const inputComputed = computed({
+    get: () => input?.value,
+    set: (val : String) => emit('update:input', val)
+})
 </script>
