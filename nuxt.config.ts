ebc83a9164c9109aba80d8a09c90f62356655c7e
pixsel1401 <pixsel1401@gmail.com>
2023-06-30 18:02:14 +0500

    start store

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,13 @@ export default defineNuxtConfig({
         '@pinia/nuxt',
         '@pinia-plugin-persistedstate/nuxt',
     ],
+    pinia: {
+        autoImports: [
+          // automatically imports `defineStore`
+          'defineStore', // import { defineStore } from 'pinia'
+          ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
+        ],
+    },
     postcss: {
         plugins: {
             tailwindcss: {},
---
+++ stores/index.ts
@@ -0,0 +1,9 @@
+// import IUser from "~/models/IUser"
+
+// export const useUserStore = defineStore('user',   {
+//     state :  () : IUser => {id : '' , name : '' },
+    
+//     actions: {
+//     //   
+//     },
+//   })
