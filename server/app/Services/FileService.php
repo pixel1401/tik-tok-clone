a9f5f311d1d43bd1d30b1a48fbd41f13b7e41a1a
pixsel1401 <pixsel1401@gmail.com>
2023-07-10 17:52:40 +0500

    start upload image

---
+++ server/app/Services/FileService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Services;
+
+use Intervention\Image\ImageManagerStatic as Image;
+use Illuminate\Http\Request;
+
+class FileService {
+    public function updateImage($model , Request $request ) {
+        $image = Image::make($request->file('image'));
+
+        if(!empty($model->image)) {
+            $currentImage = public_path() . $model->image;
+
+            if(file_exists($currentImage) && $currentImage != public_path() . '/user-placeholder.png') {
+                unlink($currentImage);
+            }
+        }
+
+        $file = $request->file('image');
+        $extension = $file->getClientOriginalExtension();
+
+        $image->crop(
+            $request->width,
+            $request->height,
+            $request->left,
+            $request->top,
+        );
+
+        
+
+
+    }
+
+}
--- server/config/app.php
+++ server/config/app.php
@@ -162,6 +162,8 @@ return [
          * Package Service Providers...
          */
 
+        Intervention\Image\ImageServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -184,6 +186,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
+        'Image' => Intervention\Image\Facades\Image::class
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
 
